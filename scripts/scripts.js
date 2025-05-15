983ba8f6fbfab14a52e1817603cf195ce476e357
Adam Jarvis <jarvisa@algonquincollege.com>
2025-05-15 12:10:32 -0400

    javascript datatypes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -26,3 +26,30 @@ console.log(ourClassNum);
 
 //const and let cannot be redeclared (or recreated)
 // let numStudents = 25; (this will also give you an error)
+
+/***********************
+    javascript datatypes
+************************/
+//storing a value of true or false is called a boolean
+let doorClosed = true;
+console.log(doorClosed);
+
+//you can create an empty variable, which will return undefined
+let whiteboardContent;
+console.log(whiteboardContent); //should give you undefined
+
+//you can use the keyword "null" (which means nothing)
+let classKids = null;
+
+//number - can be written without quotes (computer can recognize them)
+//string - is text that must be written in quotes
+
+//difference between == and ===
+const numLights = 12; //this variable is storing a number
+const numChairs = "12"; //this variable is storing a piece of text
+
+console.log(numLights == numChairs); //gives you true
+console.log(numLights === numChairs); //gives you false
+
+// two equals == checks if they are the same value
+// three equals === checks if they are the same value and same datatype
